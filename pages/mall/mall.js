f56ae39d6f367a1e585c64cfc26a15c865c90324
xushankun <lovexsk@qq.com>
2018-09-20 16:56:30 +0800

    新增tab选项自动居中【类似资讯类app的分类】

--- pages/mall/mall.js
+++ pages/mall/mall.js
@@ -1,66 +1,53 @@
 // pages/mall/mall.js
+let app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    cityList: ['南京市', '苏州', '无锡', '常州', '镇江', '南通', '泰州', '扬州', '盐城', '淮安', '宿迁', '徐州','连云港'],
+    isCityPanel: false,
+    currIndex: 0,   // city下标
+    scrollLeft: 0,
+    screenW: wx.getSystemInfoSync().windowWidth,
+    currTabId: 'active',      // scroll-id
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // -----------------------------city选择与滚动定位---------------------------
+  // 滑动的选择
+  selectCity(e) {
+    let _index = e.currentTarget.dataset.index;
+    let halfScrollW = (this.data.screenW - 88) / 2;
+    this.setData({
+      currTabId: 'active',
+      currIndex: _index,
+      scrollLeft: e.target.offsetLeft - halfScrollW
+    })
+    this.handleCurrIndex(_index);
+  },
+  // 面板的选择
+  panelSelectCity(e) {
+    let _index = e.currentTarget.dataset.index,
+      _currTabId = 'active' + _index;
+    this.setData({
+      currIndex: _index,
+      isCityPanel: false,
+      currTabId: _currTabId
+    })
+    this.handleCurrIndex(_index);
+  },
+  toggleCityPanel(e) {
+    this.setData({
+      isCityPanel: !this.data.isCityPanel
+    })
+  },
+  // -----------------------------city选择与滚动定位---------------------------
+  handleCurrIndex(index) {
+    console.log(`${index}:${this.data.cityList[index]}`);
+  },
   onLoad: function (options) {
-  
+   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  },
   onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+   
   }
 })
